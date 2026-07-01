d18da04622771ef475757deb343cfb06b4c67425
Cristian Estrella <cristianestrella.86@gmail.com>
2026-07-01 09:40:21 -0600

    remove commented code.

--- tests/LoginTests.spec.ts
+++ tests/LoginTests.spec.ts
@@ -1,17 +1,3 @@
-// import { test, expect } from '@playwright/test';
-
-// test('test', async ({ page }) => {
-//   await page.goto('https://openweathermap.org/');
-//   await page.getByRole('link', { name: 'Login' }).click();
-//   await page.getByRole('textbox', { name: 'Email' }).click();
-//   await page.getByRole('textbox', { name: 'Email' }).fill('cristianestrella.86@gmail.com');
-//   await page.getByRole('textbox', { name: 'Password' }).click();
-//   await page.getByRole('textbox', { name: 'Password' }).fill('');
-//   await page.getByRole('button', { name: 'Submit' }).click();
-//   
-// });
-
-
 // tests/auth.spec.ts
 import { test, expect } from '../src/fixtures/app.fixtures';
 import * as testData from '../test-data/user-data.json';
@@ -28,7 +14,7 @@ test.describe('Authentication Flow', () => {
       process.env.TEST_PASSWORD 
     );
 
-    await expect(homePage.successVisible()).resolves.toBe(true);
-    await expect(homePage.getSuccessText()).resolves.toContain('Signed in successfully.');
+    expect(await homePage.successVisible()).toBe(true);
+    expect(await homePage.getSuccessText()).toContain('Signed in successfully.');
   });
 });
